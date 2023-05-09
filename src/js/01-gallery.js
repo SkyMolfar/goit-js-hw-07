16ce08eb2d36fb90a7418e054398cb6bdfccaed6
SkyMolfar <skymolfars@gmail.com>
2023-05-09 22:06:41 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+function generateGalleryItem(item) {
+  let template = `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+  return template;
+}
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+gallery.innerHTML = galleryItems.map(generateGalleryItem).join("");
 
-console.log(galleryItems);
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const imageUrl = event.target.dataset.source;
+  console.log(imageUrl);
+  const instance = basicLightbox.create(
+    `<img src="${imageUrl}" width="800" height="600">`
+  );
+
+  instance.show();
+}
+
+// const instance = basicLightbox.create(`
+//   <img src="./js/gallery-items.js" width="800" height="600">
+// `);
+
+// instance.show();
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Change code below this lin
+function generateGalleryItem(item) {
+  let template = `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+</li>`;
+  return template;
+}
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+gallery.innerHTML = galleryItems.map(generateGalleryItem).join("");
 
-console.log(galleryItems);
+var lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
+
