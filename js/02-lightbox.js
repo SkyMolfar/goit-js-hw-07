c858f3a21c484cc1add5aca9209f9971fdf1b0f2
SkyMolfar <skymolfars@gmail.com>
2023-05-10 13:06:08 +0200

    src

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this lin
 function generateGalleryItem(item) {
   let template = `<li class="gallery__item">
@@ -12,5 +12,7 @@ const gallery = document.querySelector(".gallery");
 console.log(gallery);
 gallery.innerHTML = galleryItems.map(generateGalleryItem).join("");
 
-var lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
-
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
